5ac77743700341c6f19e64d8f25aaab9358ae191
goblicpl <bartosz.miziura@o2.pl>
2022-10-07 22:25:32 +0200

    Swiper Script Relocation

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,3 @@
-import Swiper from "./swiper.js"
-
-// Swiper
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: true,
-
-  slidesPerView: 1,
-  spaceBetween: 0,
-
-  centeredSlides: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-})
-
 // Observer for navbar items
 const sections = document.querySelectorAll("section")
 
